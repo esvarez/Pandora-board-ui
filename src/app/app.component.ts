04e42763256b1d7dddd8a4979e70174ee195faaa
Erick Suarez <ericksuarezdev@gmail.com>
2020-05-21 14:28:26 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'PandoraBoard';
+}
